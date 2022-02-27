0befc1fcc5d33a2678d5c5cb16a0d62499f9fc8a
eternalsooth <eternalsooth@mail.ru>
2022-02-27 17:13:17 +0300

    second commit

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -11,14 +11,26 @@ export class HeroFormComponent {
 
   powers = ['Really Smart', 'Super Flexible',
             'Super Hot', 'Weather Changer'];
+  race = ['goblin','ork','angel','demon','human'];
+  klass =['assasin','magic','healer','tank'];
+  level = 1;
 
-  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  model = new Hero(18, 'Dr IQ', this.powers[2], this.race[2],this.klass[0],this.level,'Chuck Overstreet');
 
   submitted = false;
 
   onSubmit() { this.submitted = true; }
 
-  newHero() {
-    this.model = new Hero(42, '', '');
+  checklevel(){
+
+    if (Number(this.model.level) > 100) {this.model.level = 88}
+    else if (Number(this.model.level) < 1) {this.model.level = 1}
+    
   }
+
+  newHero() {
+    this.model = new Hero(42, '', '','','',this.level);
+  }
+
+
 }
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -4,7 +4,10 @@ export class Hero {
     public id: number,
     public name: string,
     public power: string,
-    public alterEgo?: string
+    public race : string,
+    public klass : string,
+    public level : number,
+    public alterEgo?: string,
   ) {  }
 
 }
